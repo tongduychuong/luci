b69200cc86e0841d0b8242ef3bb84448e0a10f9a
Ric <ricsc@users.noreply.github.com>
2024-07-25 12:47:39 +0200

    allow to add custom written nft rules through webif

--- applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/custom.js
+++ applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/custom.js
@@ -5,13 +5,13 @@
 
 return view.extend({
 	load: function() {
-		return L.resolveDefault(fs.read('/etc/firewall.user'), '');
+		return L.resolveDefault(fs.read('/etc/nftables.d/10-custom-filter-chains.nft'), '');
 	},
 
 	handleSave: function(ev) {
 		var value = (document.querySelector('textarea').value || '').trim().replace(/\r\n/g, '\n') + '\n';
 
-		return fs.write('/etc/firewall.user', value).then(function(rc) {
+		return fs.write('/etc/nftables.d/10-custom-filter-chains.nft', value).then(function(rc) {
 			document.querySelector('textarea').value = value;
 			ui.addNotification(null, E('p', _('Contents have been saved.')), 'info');
 			fs.exec('/etc/init.d/firewall', ['restart']);
@@ -23,7 +23,7 @@ return view.extend({
 	render: function(fwuser) {
 		return E([
 			E('h2', _('Firewall - Custom Rules')),
-			E('p', {}, _('Custom rules allow you to execute arbitrary iptables commands which are not otherwise covered by the firewall framework. The commands are executed after each firewall restart, right after the default ruleset has been loaded.')),
+			E('p', {}, _('Custom ntf firewall rules.')),
 			E('p', {}, E('textarea', { 'style': 'width:100%', 'rows': 25 }, [ fwuser != null ? fwuser : '' ]))
 		]);
 	},
